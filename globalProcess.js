146863440d5b519991c3d3b42a8235ff0e29402e
tavolafourcade <tavolafourcade@gmail.com>
2021-07-15 22:06:53 -0500

    Learning about process.argv to get the information from the terminal

--- globalProcess.js
+++ globalProcess.js
@@ -1,3 +1,10 @@
-console.log(`Process object id -> ${process.pid}`)
-console.log(`Process object used to know the current version of NOde.js -> ${process.versions.node}`)
+// console.log(`Process object id -> ${process.pid}`)
+// console.log(`Process object used to know the current version of NOde.js -> ${process.versions.node}`)
 
+//Using process.argv to get information from the terminal
+//First argument -> With this we can see the full path of where the Node process lives.
+//Second argument -> Path to the current module being used.
+console.log(process.argv);
+
+//Anything we type is added to the array
+//For instance -> node globalProcess.js hola mundo
