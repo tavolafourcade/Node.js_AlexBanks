4f64687cad65489c5819088098dc726fce4a71b6
tavolafourcade <tavolafourcade@gmail.com>
2021-07-15 22:24:17 -0500

    Using process.argv to grab flags of the user input

--- globalProcess.js
+++ globalProcess.js
@@ -4,7 +4,28 @@
 //Using process.argv to get information from the terminal
 //First argument -> With this we can see the full path of where the Node process lives.
 //Second argument -> Path to the current module being used.
-console.log(process.argv);
+
+// console.log(process.argv);
 
 //Anything we type is added to the array
 //For instance -> node globalProcess.js hola mundo
+
+/*LEARNING DESTRUCTURING*/
+// const [, ,firstName, lastName] = process.argv;
+// console.log(`Your name is ${firstName} ${lastName}`);
+
+
+//$ node globalProcess.js Octavio Lafourcade
+
+/*Passing a flag to process.argv to let us know what variables we're passing*/
+// $ node globalProcess.js --user Octavio --greetings "Hello World"
+const grab = flag =>{
+  let indexAfterFlag = process.argv.indexOf(flag) + 1;
+  return process.argv[indexAfterFlag];
+}
+
+const greetings = grab('--greetings');
+const user = grab('--user');
+
+console.log(`${greetings} ${user}`)
+//$ node globalProcess.js --user Octavio --greetings "Hello Friend"
