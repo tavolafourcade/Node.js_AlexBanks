4d7badf26258d9258a9b8c8ebb2b002168890aa8
tavolafourcade <tavolafourcade@gmail.com>
2021-07-15 22:41:16 -0500

    Using process.stdout.write to have more control over the string compared to console.log

---
+++ questions.js
@@ -0,0 +1,10 @@
+/* BUILDING A SIMPLE QUESTION AND ANSWER APP */
+
+// Using these to read and write data from the terminal.
+// process.stdin
+// process.stdout
+
+
+/* process.stdout */
+process.stdout.write('Hello ');
+process.stdout.write('World \n');
