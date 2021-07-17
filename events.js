4c60ee10f49a0d12df0faed7fa3bfe3716fe93fa
tavolafourcade <tavolafourcade@gmail.com>
2021-07-17 14:20:58 -0500

    Using event emitter asynchronously

--- events.js
+++ events.js
@@ -6,5 +6,18 @@ emitter.on('customEvent', (message, user)=>{
   console.log(`${user}: ${message}`);
 });
 
-emitter.emit('customEvent', 'Hello World', 'Computer');
-emitter.emit('customEvent', 'Thats pretty cool no?', 'Octavio');
+// emitter.emit('customEvent', 'Hello World', 'Computer');
+// emitter.emit('customEvent', 'Thats pretty cool no?', 'Octavio');
+
+//Example of event emitter that is asynchronous.
+process.stdin.on('data', data =>{
+  const input = data.toString().trim(); //What user types
+
+  if (input === 'exit'){
+    emitter.emit('customEvent', 'Goodbye!', 'process')
+    process.exit()
+  }else{
+    emitter.emit('customEvent', input, 'terminal');
+  }
+  
+})
