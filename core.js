8c3f18062df78cc239d8d4dca1c602ed1a18be06
tavolafourcade <tavolafourcade@gmail.com>
2021-07-17 11:22:56 -0500

    Using path module to access uploads directory which is inside another folders

---
+++ core.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+/* Using path module to access uploads directory */
+
+const dirUploads = path.join(__dirname, 'www', 'files', 'uploads');
+
+console.log(dirUploads);
+//G:\Node.js_AlexBanks\www\files\uploads
