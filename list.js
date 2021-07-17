577795a3ff44d6934bc2e4f8dcc5335bb569e874
tavolafourcade <tavolafourcade@gmail.com>
2021-07-17 18:43:20 -0500

    Using readdir to read a file asynchronously

--- list.js
+++ list.js
@@ -1,3 +1,14 @@
 const fs = require('fs');
-const files = fs.readdirSync('./assets');
+//Blocking synchronously
+console.log('started reading files')
+//const files = fs.readdirSync('./assets');
+
+//Non blocking Asynchronously
+
+fs.readdir('./assets', (err, files)=>{
+  if (err){
+    throw err;
+  }
+  console.log('complete')
 console.log(files);
+});
