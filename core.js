881f418af96fc14b670857b346617cf4927dce04
tavolafourcade <tavolafourcade@gmail.com>
2021-07-17 11:29:33 -0500

    Using util.log which is a required module in comparison to console.log to print the name of the file including the date

--- core.js
+++ core.js
@@ -1,4 +1,7 @@
+/* CORE MODULES */
+/* PATH */
 const path = require('path');
+const util = require('util');
 
 /* Using path module to access uploads directory */
 
@@ -6,3 +9,8 @@ const dirUploads = path.join(__dirname, 'www', 'files', 'uploads');
 
 console.log(dirUploads);
 //G:\Node.js_AlexBanks\www\files\uploads
+
+/* UTIL */
+util.log(path.basename(__filename));
+util.log('^ The name of the current file')
+//util provides the date of the log instead of console.log.
