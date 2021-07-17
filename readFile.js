bb2097d26a3f6033895ecf0f26f04f39d95b0883
tavolafourcade <tavolafourcade@gmail.com>
2021-07-17 18:50:48 -0500

    Reading the file content Asynchronously

--- readFile.js
+++ readFile.js
@@ -1,5 +1,15 @@
 const fs = require('fs');
 
 /* Reading the content of the file Readme synchronously */
-const text = fs.readFileSync('./assets/Readme.md', 'utf-8')
-console.log(text)
+//const text = fs.readFileSync('./assets/Readme.md', 'utf-8')
+//console.log(text)
+
+/* Reading the content of the file Readme Asynchronously */
+fs.readFile('./assets/Readme.md', 'utf-8',(err, text)=>{
+  if (err){
+    throw err;
+  }else{
+    console.log('File contents');
+    console.log(text)
+  }
+})
