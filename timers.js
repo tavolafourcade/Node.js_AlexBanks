7c038769f3efc6160660bb9209f5342010f08fb7
tavolafourcade <tavolafourcade@gmail.com>
2021-07-16 19:18:16 -0500

    IMPROVING THE APP BY COMMUNICATING THE WAITING TIME AS A %

--- timers.js
+++ timers.js
@@ -1,8 +1,10 @@
-const waitTime = 3000;
+const waitTime = 5000;
 console.log(`setting a ${waitTime/1000} second delay`);
 const timerFinished = () => {
   //Clearing the interval as soon the waitTime is fulfilled.
   clearInterval(interval);
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
   console.log('done');
 }
 
@@ -14,7 +16,14 @@ let currentTime = 0;
 
 const incTime = () => {
   currentTime += waitInterval;
-  console.log(`waiting ${currentTime/1000} seconds`);
+  const p = Math.floor((currentTime/waitTime)*100);
+  // console.log(`waiting ${currentTime/1000} seconds`);
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0); //It moves the cursor to 0
+  process.stdout.write(`waiting...${p}%`) //Override the %  
 }
 
 const interval = setInterval(incTime,waitInterval);
+
+
+
