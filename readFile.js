d20b2add6981bf63d5579259c5e4ecfc0916890e
tavolafourcade <tavolafourcade@gmail.com>
2021-07-17 19:08:33 -0500

    Writing a file with the fs.writeFile function

--- readFile.js
+++ readFile.js
@@ -5,11 +5,22 @@ const fs = require('fs');
 //console.log(text)
 
 /* Reading the content of the file Readme Asynchronously */
-fs.readFile('./assets/Readme.md', 'utf-8',(err, text)=>{
+// fs.readFile('./assets/Readme.md', 'utf-8',(err, text)=>{
+//   if (err){
+//     throw err;
+//   }else{
+//     console.log('File contents');
+//     console.log(text)
+//   }
+// })
+
+/* Reading binary files as images */
+fs.readFile('./assets/alex.jpg',(err, img)=>{
   if (err){
-    throw err;
+    console.log(`An error has ocurred: ${err.message}`);
+    process.exit();
   }else{
     console.log('File contents');
-    console.log(text)
+    console.log(img)
   }
 })
